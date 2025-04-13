0128e16b6315617a706f50a472ac6d13e56ad23b
Taufiqul Islam <tosunbd@gmail.com>
2025-04-13 16:01:51 +0600

    First Initialization

---
+++ HubServer/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace HubServer
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var userId = string.Empty;
+            var userName = string.Empty;
+
+            userId = Context.GetHttpContext()?.Request.Query["userId"].ToString();
+            userName = Context.GetHttpContext()?.Request.Query["userName"].ToString();
+
+            Console.WriteLine($"User {userName} with Id {userId} has connected.");
+
+            await Clients.Caller.SendAsync("ReceiveSystemMessage", $"Hi {Context.ConnectionId}, you have just connected");
+        }
+    }
+}
