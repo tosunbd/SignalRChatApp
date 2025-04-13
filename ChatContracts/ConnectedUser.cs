a5025ffdcec6dbc74b42d462980e5ea1b179973a
tosunbd <tosunbd@gmail.com>
2025-04-13 20:41:10 +0600

    Clean up ignored files from repo

---
+++ ChatContracts/ConnectedUser.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatContracts
+{
+    public class ConnectedUser
+    {
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? ConnectionId { get; set; }
+        //public List<string> SystemMessages { get; set; }
+    }
+}
